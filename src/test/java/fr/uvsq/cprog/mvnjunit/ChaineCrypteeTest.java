18add59084a5ac8c946810ce7156fece97d679f9
Mohamed <medmohdz181@gmail.com>
2025-10-02 12:34:48 +0200

    reponse 8

--- src/test/java/fr/uvsq/cprog/mvnjunit/ChaineCrypteeTest.java
+++ src/test/java/fr/uvsq/cprog/mvnjunit/ChaineCrypteeTest.java
@@ -6,9 +6,38 @@ import static org.junit.Assert.assertEquals;
 public class ChaineCrypteeTest {
 
     @Test
-    public void testCrypteDecrypte() {
-        ChaineCryptee chaine = ChaineCryptee.deEnClair("BONJOUR", 3);
-        assertEquals("ERQMRXU", chaine.crypte());
-        assertEquals("BONJOUR", chaine.decrypte());
+    public void testChiffrementDecalageNegatif() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("BONJOUR", -3);
+        assertEquals("YLKGLRO", c.crypte());
+    }
+
+    @Test
+    public void testDechiffrementDecalageNegatif() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("YLKGLRO", -3);
+        assertEquals("BONJOUR", c.decrypte());
+    }
+
+    @Test
+    public void testMajusculesEtEspaces() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("HELLO WORLD", 5);
+        assertEquals("MJQQT BTWQI", c.crypte());
+    }
+
+    @Test
+    public void testCaracteresNonAlphabetiques() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("BONJOUR123!@#", 3);
+        assertEquals("ERQMRXU123!@#", c.crypte());
+    }
+
+    @Test
+    public void testChaineVide() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("", 3);
+        assertEquals("", c.crypte());
+    }
+
+    @Test
+    public void testDecalageSuperieur26() {
+        ChaineCryptee c = ChaineCryptee.deEnClair("BONJOUR", 29); // 29 % 26 = 3
+        assertEquals("ERQMRXU", c.crypte());
     }
 }
